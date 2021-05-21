b1349d8cd53f740d88bda1f68369ce166e2cb967
NikunjR0511 <nikunjr@quantira.com>
2021-05-21 09:50:56 +0000

    First commit

---
+++ cleanup.bat
@@ -0,0 +1,6 @@
+set targetdir=C:\BackTest_Engin\Backtest\backtest\static\backtest-file\input
+del /q %targetdir%\*
+set targetdir=C:\BackTest_Engin\Backtest\backtest\static\backtest-file\output
+del /q %targetdir%\*
+
+
